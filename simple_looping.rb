5b8322ef2ad2905a302d4a2ac83092cbe4007568
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-15 01:12:05 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def loop_iterator(number_of_times)
     counter += 1 
     puts phrase
     puts "test"
-    break if counter == number_of_times
+    break if counter >= number_of_times
   end
 end
 
