536e42ceffec8d284d1257f5f5941daa287ce59c
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-15 00:44:41 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,10 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0 
   loop {
+    puts phrase
+    counter += 1 
+    break 
+  }
   
 end
 
